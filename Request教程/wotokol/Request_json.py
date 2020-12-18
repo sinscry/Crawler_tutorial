772c50b9e470179a67f66719ec233b8106d5dc66
sinscry <sinscryw@gmail.com>
2020-12-18 11:07:53 +0800

    可转债json

--- Request教程/wotokol/Request_json.py
+++ Request教程/wotokol/Request_json.py
@@ -16,7 +16,7 @@ class Pipeline:
 
 if __name__ == '__main__':
     #请求地址
-    url = "https://www.wotokol.com/index.php/api/search/youtube?link=&price=&subscriber=0-100000&kol_country=&kol_sex=&category=820&s_country=142&s_sex=&s_age=&s_operating=&s_terminal=&wares_key=&is_brand=&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDQyODYyMjcsImV4cCI6MTYwNDM0NjIyNywidXNlcl9pZCI6MTU5NCwicGhvbmUiOiIxMzkyNjE3ODU3NyIsIm5hbWUiOm51bGx9.630M6TeHTwr2qHSsgDkArS8VM6BE1mM1E7D2dWh-obM&page=1&limit=10000"    #发送get请求
+    # url = "https://www.wotokol.com/index.php/api/search/youtube?link=&price=&subscriber=0-100000&kol_country=&kol_sex=&category=820&s_country=142&s_sex=&s_age=&s_operating=&s_terminal=&wares_key=&is_brand=&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDQyODYyMjcsImV4cCI6MTYwNDM0NjIyNywidXNlcl9pZCI6MTU5NCwicGhvbmUiOiIxMzkyNjE3ODU3NyIsIm5hbWUiOm51bGx9.630M6TeHTwr2qHSsgDkArS8VM6BE1mM1E7D2dWh-obM&page=1&limit=10000"    #发送get请求
     r = requests.get(url)
     #获取返回的json数据
     data_list = r.json()['data']
